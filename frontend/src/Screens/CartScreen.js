4bb3ca2e39ba2031b63699e2fdff2ed8728925f9
jaco9419 <jaco.9419@gmail.com>
2020-08-13 11:49:37 -0300

    Create Cart Screen, Actions and Reducers files

---
+++ frontend/src/Screens/CartScreen.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+function CartScreen (props) {
+    const productId = props.match.params.id;
+    const qty = props.location.search ? Number(props.location.search.split('=')[1]) : 1;
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(product, qty));
+        }
+    }, [])
+
+
+    return <div>
+    Cart Screen
+    </div>
+}
+
+export default CartScreen;
