7e7728d5c9340db9f9a67a5d36b09576631bc8f2
jaco9419 <jaco.9419@gmail.com>
2020-08-17 22:17:12 -0300

    Finish Styling

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,66 +1,72 @@
 import image from './images/d1.jpg';
 
 export default {
-    products:[
+    products: [
         {
-        _id: '1',
-        name: 'Slim Shirt',
-        category: 'Shirts',
-        image: image,
-        price: 60,
-        brand: 'Nike',
-        rating: 4.5,
-        numReviews: 10
-    },
-    {
-        _id: '2',
-        name: 'Fit Shirt',
-        category: 'Shirts',
-        image: image,
-        price: 50,
-        brand: 'Nike',
-        rating: 4,
-        numReviews: 8
-    },
-    {
-        _id: '3',
-        name: 'Best Pants',
-        category: 'Pants',
-        image: image,
-        price: 70,
-        brand: 'Nike',
-        rating: 5,
-        numReviews: 9
-    },
-    {
-        _id: '4',
-        name: 'Shirt',
-        category: 'Shirts',
-        image: image,
-        price: 20,
-        brand: 'Lacoste',
-        rating: 5,
-        numReviews: 9
-    },
-    {
-        _id: '5',
-        name: 'Jeans',
-        category: 'Pants',
-        image: image,
-        price: 40,
-        brand: 'Forever 21',
-        rating: 3,
-        numReviews: 11
-    },
-    {
-        _id: '6',
-        name: 'Joggings',
-        category: 'Pants',
-        image: image,
-        price: 20,
-        brand: 'Adidas',
-        rating: 4,
-        numReviews: 13
-    }
-]
-}
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '../images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '../images/d2.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 8,
+            countInStock: 0,
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '../images/d3.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 9,
+            countInStock: 6,
+        },
+        {
+            _id: '4',
+            name: 'Shirt',
+            category: 'Shirts',
+            image: '../images/d4.jpg',
+            price: 20,
+            brand: 'Lacoste',
+            rating: 5,
+            numReviews: 9,
+            countInStock: 20,
+        },
+        {
+            _id: '5',
+            name: 'Jeans',
+            category: 'Pants',
+            image: '../images/d5.jpg',
+            price: 40,
+            brand: 'Forever 21',
+            rating: 3,
+            numReviews: 11,
+            countInStock: 6,
+        },
+        {
+            _id: '6',
+            name: 'Joggings',
+            category: 'Pants',
+            image: '../images/d6.jpg',
+            price: 20,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 13,
+            countInStock: 6,
+        },
+    ],
+};
