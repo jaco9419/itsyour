3764fc520b0de5b3e1d478b659fcc74fb3d7ffd1
jaco9419 <jaco.9419@gmail.com>
2020-08-17 22:46:59 -0300

    Finish Styling

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -32,17 +32,21 @@ function HomeScreen(props) {
                             />
                         </Link>
 
-                        <div className="product-name">
-                            <Link to={'/product/' + product._id}>
-                                {product.name}
-                            </Link>
+                        <div className="product-description">
+                            <div className="product-name">
+                                <Link to={'/product/' + product._id}>
+                                    {product.name}
+                                </Link>
+                            </div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">${product.price}</div>
+                            <div className="product-rating">
+                                {product.rating} Stars ({product.numReviews}{' '}
+                                Reviews)
+                            </div>
                         </div>
-                        <div className="product-brand">{product.brand}</div>
-                        <div className="product-price">${product.price}</div>
-                        <div className="product-rating">
-                            {product.rating} Stars ({product.numReviews}{' '}
-                            Reviews)
-                        </div>
+
+                        
                     </div>
                 </li>
             ))}
